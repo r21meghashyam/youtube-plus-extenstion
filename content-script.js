ce2cdf4af9fcfa679e5ad270ca12e666c6c631db
Meghashyam Bhandary <r21meghashyam@gmail.com>
2019-06-12 13:01:59 +0530

    first commit

--- content-script.js
+++ content-script.js
@@ -1,18 +1,25 @@
-if (window.location.host == "www.hotstar.com") {
-  setInterval(() => {
-    let obj = JSON.parse(localStorage.getItem("sportlivetimeentries"));
-    if (obj) {
-      let nofreeluncheon = JSON.parse(obj.nofreeluncheon);
-      if (nofreeluncheon)
-        Object.keys(nofreeluncheon).forEach(id => {
-          let elapsedTime = nofreeluncheon[id].elapsedTime;
-          if (elapsedTime > 300000) {
-            localStorage.clear();
-            window.location =
-              window.location.origin + window.location.pathname + "?lang=eng";
-          }
-        });
+
+let old = [];
+let db = JSON.parse(localStorage.getItem("youtube-plus"))||[];
+setInterval(()=>{
+  let tiles = document.querySelectorAll('ytd-grid-video-renderer,ytd-compact-video-renderer');
+  tiles=Array.from(tiles).filter(tile=>(tile.getBoundingClientRect().y<innerHeight*0.8))
+  
+  tiles.forEach(tile=>{
+    //console.log(tile,tile.offsetTop,(scrollY+innerHeight)/2);
+    const link = tile.querySelector("a").href;
+    if(old.find(_link=>link==_link))
+      return;
+    if(db.find(_link=>link==_link)){
+      tile.style.opacity="0.1";
     }
-  }, 1000);
-  console.log("HOTSTAR HACK RUNNING");
-}
+    else{
+      db.push(link);
+      old.push(link);
+      console.log(link+" added to list");
+    }
+  });
+localStorage.setItem("youtube-plus",JSON.stringify(db));
+},1000);
+console.log("Youtube Plus running..."+db.length+" videos recorded.");
+
